17361ed634d697e61305f3611dd1810c7faa4778
pengyuhe <pyhejhu@gmail.com>
2023-01-12 20:26:18 -0500

    solution for 1.3, 1.4, 1.5

---
+++ chapter1/1_3_1_4_1_5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(){
+	// Exercise 1.2
+	std::cout<<"Hello, World"<<std::endl;
+
+    // Exercise 1.3
+    int v1 = 0, v2 = 0;
+    std::cin>>v1>>v2;
+	std::cout << "The multiply of " << v1 << " and " << v2 << " is " << v1 * v2 << std::endl;
+
+
+	// Exercise 1.4
+	std::cout << "The multiply of " << v1 << " and " << v2 ;
+	std::cout<< " is " << v1 * v2 << std::endl;
+}
